bfcd1caf3e204e07d2aea481e3fb55801381a37e
sravanthigullapalli22 <lakshmisravanthi651@gmail.com>
2026-06-19 16:32:43 +0530

    if-else

---
+++ basics.py
@@ -0,0 +1,31 @@
+
+name="sravanthi"
+age=20
+city="ongole"
+career_goal="software developer"
+clg="Dhanalakshmi srinivasan university"
+department="AI-DS"
+passed_out=2026
+print(name)
+print(age)
+print(city)
+print(career_goal)
+print(clg)
+print(department)
+print(passed_out)
+python_class="9:00 AM Python"
+aptitude_class="11:00 AM Aptitude"
+soft_skills="12:00 Pm Softskills"
+pc_session="04:00 Pm Practice session"
+print(python_class)
+print(aptitude_class)
+print(soft_skills)
+print(pc_session)
+Institute_name="codeganan"
+traineer_name="hari krihna"
+course_name="PYTHON FULL STACK"
+Batch=57
+print(Institute_name)
+print(traineer_name)
+print(course_name)
+print(Batch)
